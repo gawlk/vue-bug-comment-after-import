25b303f41778cd92e1c708599f3eb4fbeba456ee
Kevin Gawlik <kevin@gawlik.io>
2020-09-12 11:01:37 +0200

    initial: commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld msg="Hello Vue 3.0 + Vite" />
+</template>
+
+<script setup>
+import mediasoup from 'mediasoup-client' // remove this comment and it'll work
+
+import HelloWorld from './components/HelloWorld.vue'
+
+// import mediasoup from 'mediasoup-client' // Here is works even with the comment
+
+export default {
+  components: {
+    HelloWorld
+  }
+}
+</script>
